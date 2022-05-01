c9c5e1e3690b701e2c09eaab7411393518642c5c
Stefan malewski <smalewsk@dcc.uchile.cl>
2022-04-30 23:17:25 -0400

    config: Dev scripts for nix-shell

--- shell.nix
+++ shell.nix
@@ -4,7 +4,14 @@ with pkgs;
 mkShell {
   nativeBuildInputs = (with ocaml-ng.ocamlPackages_4_13; [
     ocaml stdio 
-    ppx_expect bisect_ppx ppx_jane
+    bisect_ppx ppx_jane
     dune_2 menhir merlin ocamlformat findlib ocaml-lsp  
   ]) ++ [inotify-tools];
+
+  shellHook = ''
+    alias dune-watch="dune build -w"
+    alias dune-test="dune runtest"
+    alias dune-coverage="dune runtest --instrument-with bisect_ppx --force; bisect-ppx-report html; bisect-ppx-report summary"
+    alias dune-run="dune exec gcic"
+  '';
 }
