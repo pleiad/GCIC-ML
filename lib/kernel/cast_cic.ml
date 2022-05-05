97d6f90ca2309ba8ba76e60998c436a86d229525
Tomas <tomdtron@gmail.com>
2022-05-05 17:09:58 -0400

    fix: Adjust canonical for ? and previous bug

--- lib/kernel/cast_cic.ml
+++ lib/kernel/cast_cic.ml
@@ -110,7 +110,9 @@ let is_germ i : term -> bool = function
 (** Checks if a term is in neutral form *)
 let rec is_neutral : term -> bool = function
   | Var _ -> true
-  | App (t, _) | Unknown t | Err t
+  | App (t, _) 
+  | Unknown t 
+  | Err t
   | Cast {source=Unknown (Universe _); target=_;      term=t}
   | Cast {source=Universe _;           target=t;      term=_}
   | Cast {source=Prod _;               target=Prod _; term=t}
@@ -119,16 +121,17 @@ let rec is_neutral : term -> bool = function
   | _ -> false
 
   (** Checks if a type t makes ?_t or err t canonical *)
-let is_unknown_or_error_canonical : term -> bool = function
+let is_unknown_or_error_canonical (t : term) : bool = 
+  match t with
   | Universe _ | Unknown (Universe _) | Err (Universe _) -> true
-  | _ -> false
+  | _ -> is_neutral t
 
 (** Checks if a term is in canonical form *)
 let is_canonical : term -> bool = function
   | Universe _ | Lambda _ | Prod _ -> true
   | Unknown t -> is_unknown_or_error_canonical t
   | Err t -> is_unknown_or_error_canonical t
-  | Cast {source=ty; target=Unknown (Universe i); term=_} -> is_germ i ty
+  | Cast {source=ty; target=Unknown (Universe i); term=_} when is_germ i ty -> true
   | t -> is_neutral t
 
 
--- test/kernel/test_castcic.ml
+++ test/kernel/test_castcic.ml
@@ -1,11 +1,12 @@
 open! Kernel.Cast_cic
+open Arbitrary
 
 (* pprint_foo : Format.formatter -> foo -> unit *)
-let pprint_head ppf =  function
-| HProd -> Fmt.pf ppf "HProd"
-| HUniverse i -> Fmt.pf ppf "HUniverse: %d" i
+let pprint_head ppf = function
+  | HProd -> Fmt.pf ppf "HProd"
+  | HUniverse i -> Fmt.pf ppf "HUniverse: %d" i
 
-let head_eq a b = (a=b)
+let head_eq a b = a = b
 
 (* Pass these two functions in to get a value of type foo testable *)
 let head_testable = Alcotest.testable pprint_head head_eq
@@ -15,5 +16,28 @@ let tests_head () =
     "head of Var is Error" (Error "invalid term to get head constructor")
     (head (Var (Name.of_string "x")))
 
+let tests_is_neutral () =
+  Alcotest.(check bool)
+    "cast from error is neutral"
+    true
+    (is_neutral
+       (Cast
+          {
+            source = Err (Var (Name.of_string "_"));
+            target = Unknown (Universe 2);
+            term = Universe 57;
+          }))
+
+let neutrals_are_canonical =
+  QCheck.(
+    Test.make ~count:1000 ~name:"neutral terms are canonical"
+      arbitrary_cast_cic_term (fun t ->
+        assume (is_neutral t);
+        is_canonical t))
+
 let tests =
-    [ ( "head", `Quick, tests_head ) ]
+  [
+    ("head", `Quick, tests_head);
+    ("is_neutral", `Quick, tests_is_neutral);
+    QCheck_alcotest.to_alcotest neutrals_are_canonical;
+  ]
