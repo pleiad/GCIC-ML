bdf90fdef9b4bfc85b7d2dfa167b3da078ee966a
Tomas <tomdtron@gmail.com>
2022-05-05 12:20:15 -0400

    refactor: Rename in typing module

--- lib/kernel/typing.mli
+++ lib/kernel/typing.mli
@@ -3,9 +3,19 @@
 open Cast_cic
 
 type type_error
-val error_msg : type_error -> string
+(** A typing error *)
 
-val infering : context -> term -> (term, type_error) result
-val checking : context -> term -> term -> (unit, type_error) result
-val infering_prod : context -> term -> (Name.t * term * term, type_error) result
-val infering_univ : context -> term -> (int, type_error) result
+val error_msg : type_error -> string
+(** Extracts the error message or description *)
+
+val infer_type : context -> term -> (term, type_error) result
+(** Infers the type of the given term *)
+
+val check_type : context -> term -> term -> (unit, type_error) result
+(** Checks that the term has the given type *)
+
+val infer_prod : context -> term -> (Name.t * term * term, type_error) result
+(** Constrained inference, where the inferred type must be convertible to a product *)
+
+val infer_univ : context -> term -> (int, type_error) result
+(** Constrained inference, where the inferred type must be convertible to a universe *)
