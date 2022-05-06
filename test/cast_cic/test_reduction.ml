dda51e4e0750c7d574bc48a82d218cfab1f230cf
Tomas <tomdtron@gmail.com>
2022-05-05 22:46:34 -0400

    test: Add beta identity + down- cases

--- test/cast_cic/test_reduction.ml
+++ test/cast_cic/test_reduction.ml
@@ -5,8 +5,7 @@ let id = Ast.Name.of_string "x"
 
 let strong_normalization =
   QCheck.(
-    Test.make ~count:1000 ~name:"strong normalization" Arbitrary.term
-      (fun t ->
+    Test.make ~count:1000 ~name:"strong normalization" Arbitrary.term (fun t ->
         assume (Typing.infer_type Context.empty t |> Result.is_ok);
         Reduction.reduce t |> Ast.is_canonical))
 
@@ -25,33 +24,50 @@ let subject_reduction_empty_ctx =
 let progress_empty_ctx =
   let ctx = Context.empty in
   QCheck.(
-    Test.make ~count:1000 ~name:"progress in empty ctx" Arbitrary.term
-      (fun t ->
+    Test.make ~count:1000 ~name:"progress in empty ctx" Arbitrary.term (fun t ->
         assume (Typing.infer_type ctx t |> Result.is_ok);
-        (Ast.is_canonical t || (Reduction.step ctx t |> Result.is_ok))
-        ))
+        Ast.is_canonical t || Reduction.step ctx t |> Result.is_ok))
 
-(* TODO: Improve this with proper testable  *)
+let test_app_reduce () =
+  let open Ast in
+  Alcotest.check Testable.term "beta identity" (Universe 0)
+    (Reduction.reduce
+       (App (Lambda { id; dom = Universe 1; body = Var id }, Universe 0)))
+
 let test_unknown_reduce () =
   let open Ast in
-  let expected = Lambda { id; dom = Universe 5; body = Unknown (Universe 0) } in
-  Alcotest.check Testable.term
-    "reduce prod unknown to lambda" expected
+  Alcotest.check Testable.term "Prod-Unk universe"
+    (Lambda { id; dom = Universe 5; body = Unknown (Universe 0) })
     (Reduction.reduce
-         (Unknown (Prod { id; dom = Universe 5; body = Universe 0 })))
+       (Unknown (Prod { id; dom = Universe 5; body = Universe 0 })));
+  Alcotest.check Testable.term "Down-Unk universe" (Unknown (Universe 0))
+    (Reduction.reduce
+       (Cast
+          {
+            source = Unknown (Universe 0);
+            target = Universe 0;
+            term = Unknown (Unknown (Universe 1));
+          }))
 
 let test_error_reduce () =
   let open Ast in
-  let expected = Lambda { id; dom = Universe 5; body = Err (Universe 0) } in
-  Alcotest.check Testable.term
-    "reduce prod error to lambda" expected
-     (Reduction.reduce
-       (Err (Prod { id; dom = Universe 5; body = Universe 0 })))
+  Alcotest.check Testable.term "Prod-Err universe"
+    (Lambda { id; dom = Universe 5; body = Err (Universe 0) })
+    (Reduction.reduce (Err (Prod { id; dom = Universe 5; body = Universe 0 })));
+  Alcotest.check Testable.term "Down-Err universe" (Err (Universe 0))
+    (Reduction.reduce
+       (Cast
+          {
+            source = Unknown (Universe 0);
+            target = Universe 0;
+            term = Err (Unknown (Universe 1));
+          }))
 
 let tests =
   [
     ("reduce unknown", `Quick, test_unknown_reduce);
     ("reduce error", `Quick, test_error_reduce);
+    ("reduce app", `Quick, test_app_reduce);
     QCheck_alcotest.to_alcotest strong_normalization;
     QCheck_alcotest.to_alcotest subject_reduction_empty_ctx;
     QCheck_alcotest.to_alcotest progress_empty_ctx;
