a015d7466bf48921473ef1c0e8bd54d14b0443b4
Stefan malewski <smalewsk@dcc.uchile.cl>
2022-05-05 20:46:59 -0400

    refactor: Testable module

--- test/kernel/test_castcic.ml
+++ test/kernel/test_castcic.ml
@@ -1,18 +1,9 @@
 open! Kernel.Cast_cic
 open Arbitrary
 
-(* pprint_foo : Format.formatter -> foo -> unit *)
-let pprint_head ppf = function
-  | HProd -> Fmt.pf ppf "HProd"
-  | HUniverse i -> Fmt.pf ppf "HUniverse: %d" i
-
-let head_eq a b = a = b
-
-(* Pass these two functions in to get a value of type foo testable *)
-let head_testable = Alcotest.testable pprint_head head_eq
 
 let tests_head () =
-  Alcotest.(check (result head_testable string))
+  Alcotest.(check (result Testable.head string))
     "head of Var is Error" (Error "invalid term to get head constructor")
     (head (Var (Name.of_string "x")))
 
---
+++ test/kernel/testable.ml
@@ -0,0 +1,15 @@
+open Kernel
+
+let head =
+  let open Cast_cic in
+  let pprint_head ppf = function
+    | HProd -> Fmt.pf ppf "HProd"
+    | HUniverse i -> Fmt.pf ppf "HUniverse: %d" i in
+  let head_eq a b = a = b in
+  Alcotest.testable pprint_head head_eq
+
+let term =
+  let open Cast_cic in
+  let pprint_term ppf t = Format.pp_print_string ppf (to_string t) in
+  let term_eq = alpha_equal in
+  Alcotest.testable pprint_term term_eq
