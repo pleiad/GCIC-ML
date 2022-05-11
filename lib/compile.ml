6e73beab6a58d9000b5a9c9fd8ff77176d88afaf
Tomas <tomdtron@gmail.com>
2022-05-11 16:18:17 -0400

    feat: Change compile to actually elaborate & reduce

--- lib/compile.ml
+++ lib/compile.ml
@@ -1,10 +1,24 @@
 open Parsing
-(* open Lexer *)
+open Common
+
+
+let rec of_parsed_term (t : Parsing.Ast.term) : Kernel.Ast.term =
+  match t with 
+  | Var x -> Var x 
+  | Universe i -> Universe i 
+  | App (t, u) -> App (of_parsed_term t, of_parsed_term u)
+  | Lambda (id, dom, body) -> Lambda {id; dom= of_parsed_term dom; body= of_parsed_term body}
+  | Prod (id, dom, body) -> Prod {id; dom= of_parsed_term dom; body= of_parsed_term body}
+  | Unknown i -> Unknown i
 
 (** Compiles a string and returns the stringified version of the AST *)
 let compile (line : string) =
   let linebuf = Lexing.from_string line in
   match Lex_and_parse.parse_term linebuf with
-  | Ok term -> Ast.to_string term
+  | Ok term -> 
+    let open Cast_cic in
+    (match of_parsed_term term |> Elaboration.elaborate Context.empty with 
+    | Ok (elab, _) -> Reduction.reduce elab |> Ast.to_string 
+    | Error _ -> "failed to elaborate")    
   | Error e -> e
 
