29bc4ae88b5b5a335b115022d8c519db4da54909
Tomas <tomdtron@gmail.com>
2022-05-16 10:48:47 -0400

    refactor: Extract functions for each cmd

--- lib/vernac/ast.ml
+++ lib/vernac/ast.ml
@@ -52,27 +52,34 @@ let mk_elaboration_error (fn : command -> string -> execute_error) cmd
   let reason = Cast_cic.Elaboration.string_of_error e in
   fn cmd reason
 
-let execute cmd : (cmd_result, execute_error) result =
+let execute_eval term : (cmd_result, execute_error) result =
+  let open Cast_cic in
+  match Elaboration.elaborate Context.empty term with
+  | Ok (elab_term, _) -> Ok (Reduction (Reduction.reduce elab_term))
+  | Error e -> Error (mk_elaboration_error reduction_error (Eval term) e)
+
+let execute_check term ty : (cmd_result, execute_error) result =
   let open Cast_cic.Elaboration in
-  let open Cast_cic.Reduction in
   let open Cast_cic.Typing in
   let empty_ctx = Context.empty in
-  match cmd with
-  | Eval t ->
-      elaborate empty_ctx t
-      |> Result.map (fun (elab, _) -> Reduction (reduce elab))
-      |> Result.map_error (mk_elaboration_error reduction_error cmd)
-  | Check (t, ty) -> (
-      match elaborate empty_ctx t with
-      | Ok (elab_term, _) -> (
-          match elaborate empty_ctx ty with
-          | Ok (expected_ty, _) -> (
-              match check_type empty_ctx elab_term expected_ty with
-              | Ok _ -> Ok Check
-              | Error e -> Error (error_msg e |> checking_error cmd))
-          | Error e -> Error (mk_elaboration_error checking_error cmd e))
+  let cmd : command = Check (term, ty) in
+  match elaborate empty_ctx term with
+  | Ok (elab_term, _) -> (
+      match elaborate empty_ctx ty with
+      | Ok (expected_ty, _) -> (
+          match check_type empty_ctx elab_term expected_ty with
+          | Ok _ -> Ok Check
+          | Error e -> Error (error_msg e |> checking_error cmd))
       | Error e -> Error (mk_elaboration_error checking_error cmd e))
-  | Elaborate t -> (
-      match elaborate empty_ctx t with
-      | Ok (elab_term, _) -> Ok (Elaboration elab_term)
-      | Error e -> Error (mk_elaboration_error elaboration_error cmd e))
+  | Error e -> Error (mk_elaboration_error checking_error cmd e)
+
+let execute_elab term : (cmd_result, execute_error) result =
+  match Cast_cic.Elaboration.elaborate Context.empty term with
+  | Ok (elab_term, _) -> Ok (Elaboration elab_term)
+  | Error e -> Error (mk_elaboration_error elaboration_error (Elaborate term) e)
+
+let execute cmd : (cmd_result, execute_error) result =
+  match cmd with
+  | Eval t -> execute_eval t
+  | Check (t, ty) -> execute_check t ty
+  | Elaborate t -> execute_elab t
