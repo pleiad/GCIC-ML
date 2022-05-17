6721b112b46f4d4333d86bb0ce810ab24556eb15
Tomas <tomdtron@gmail.com>
2022-05-17 12:47:42 -0400

    doc: Add some comments on the placement of ctx

--- lib/cast_cic/context.mli
+++ lib/cast_cic/context.mli
@@ -1,10 +1,12 @@
 (** This module specifies the context for CastCIC (ie. Gamma) *)
 
-(** The mapping from identifiers to some type *)
+(* FIXME: This should probably go in another place, it's not contained to CastCIC
+   (maybe in Common?). Since it's so small and only CastCIC uses it, it'll live here 
+   for now *)
 module NameMap : Map.S with type key = Common.Id.Name.t
+(** The mapping from identifiers to some type *)
 
+type context = Ast.term NameMap.t
 (** The specific mapping from identifiers to terms *)
-type context = Ast.term NameMap.t 
 
-(** Returns the stringified version of the context *)
-val string_of_context : context -> string
+val string_of_context : context -> string(** Returns the stringified version of the context *)
