7d042947b6f0f33a3983624111025355584d7f88
Stefan malewski <smalewsk@dcc.uchile.cl>
2022-05-18 19:25:24 -0400

    config: Nix alias

--- shell.nix
+++ shell.nix
@@ -14,5 +14,6 @@ mkShell {
     alias dune-test="dune runtest"
     alias dune-coverage="dune runtest --instrument-with bisect_ppx --force; bisect-ppx-report html; bisect-ppx-report summary"
     alias dune-run="dune exec gcic"
+    alias update-parser-errors="sh lib/parsing/update_error_msgs.sh"
   '';
 }
