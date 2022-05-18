f9aaa52f495a188904ae0ab31a3c6ec361d07151
Éric Tanter <etanter@gmail.com>
2022-05-18 18:52:55 -0400

    nicer repl display
    fix format

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,15 @@
 open Gcic
 open Stdio
 
+let get_input () =
+  print_string "> ";
+  flush Out_channel.stdout;
+  In_channel.input_line In_channel.stdin
+
 let rec eval = function
   | None -> ()
   | Some x ->
     print_endline (Compile.compile x);
-    eval (In_channel.input_line In_channel.stdin)
+    eval (get_input ())
 
-let () = eval (In_channel.input_line In_channel.stdin)
+let () = eval (get_input ())
