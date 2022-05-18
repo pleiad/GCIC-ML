16b74a3ba3acc377c48639801e2ff73b8b0576c6
Tomas <tomdtron@gmail.com>
2022-05-18 17:12:20 -0400

    doc: Add an extra comment to the script

--- lib/parsing/update_error_msgs.sh
+++ lib/parsing/update_error_msgs.sh
@@ -3,6 +3,9 @@
 # Based off the example Makefile in Menhir's repo
 # https://gitlab.inria.fr/fpottier/menhir/-/blob/master/demos/calc-syntax-errors/Makefile.messages.maintenance
 
+# For simplicity sake, this script is meant to be run inside 
+# the parsing folder. 
+
 dune exec menhir -- parser.mly --list-errors > parserMessages.auto.messages 	
 dune exec menhir -- parser.mly --merge-errors parserMessages.auto.messages --merge-errors parserMessages.messages > parserMessages.merged 	
 mv parserMessages.merged parserMessages.messages
