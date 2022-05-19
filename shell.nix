5a459e4d66179e51067e41ebb426bb771d50f8f7
Stefan malewski <smalewsk@dcc.uchile.cl>
2022-05-18 22:36:36 -0400

    config: Readline wrapper for repl

--- shell.nix
+++ shell.nix
@@ -7,12 +7,12 @@ mkShell {
     menhir menhirLib sedlex_2
     dune_2 merlin ocamlformat findlib ocaml-lsp  utop
     alcotest qcheck qcheck-alcotest bisect_ppx
-  ]) ++ [inotify-tools];
+  ]) ++ [inotify-tools rlwrap];
 
   shellHook = ''
     alias dune-watch="dune build -w"
     alias dune-test="dune runtest"
     alias dune-coverage="dune runtest --instrument-with bisect_ppx --force; bisect-ppx-report html; bisect-ppx-report summary"
-    alias dune-run="dune exec gcic"
+    alias dune-run="rlwrap -m dune exec gcic"
   '';
 }
