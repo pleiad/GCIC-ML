994cb476a2c2d38d4a947dae192e663043ead907
Stefan malewski <smalewsk@dcc.uchile.cl>
2022-05-29 16:12:01 -0400

    refactor: rm duplicated script

--- updateErrorMsgs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cd lib/parsing
-dune exec menhir -- parser.mly --list-errors > parserMessages.auto.messages 	
-dune exec menhir -- parser.mly --merge-errors parserMessages.auto.messages --merge-errors parserMessages.messages > parserMessages.merged 	
-mv parserMessages.merged parserMessages.messages
-rm -f parserMessages.auto.messages
