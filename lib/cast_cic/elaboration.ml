67edbcc5327c342a1fcc0c063cd8fefe4f5bd5c3
Tomas <tomdtron@gmail.com>
2022-07-20 17:28:43 -0400

    refactor: Cleanup are consistent

--- lib/cast_cic/elaboration.ml
+++ lib/cast_cic/elaboration.ml
@@ -57,67 +57,67 @@ let string_of_error err =
 (** Type alias for the elaboration result  *)
 type elaboration = Ast.term * Ast.term
 
-let alpha_pred_branch pred b1 b2 =
-  let open Ast in
-  if List.compare_lengths b1.ids b2.ids = 0
-  then (
-    let new_ids = List.map (fun _ -> Var (new_identifier ())) b1.ids in
-    let ids1 = List.combine b1.ids new_ids in
-    let ids2 = List.combine b1.ids new_ids in
-    let subst_body body ids =
-      List.fold_left (fun body (old_id, new_id) -> subst1 old_id new_id body) body ids
-    in
-    let subst_body1 = subst_body b1.term ids1 in
-    let subst_body2 = subst_body b2.term ids2 in
-    b1.ctor = b2.ctor && pred subst_body1 subst_body2)
-  else false
+(** Checks if two terms are consistent *)
+let rec are_consistent reduce t1 t2 : bool =
+  let res =
+    let* t1_red = reduce t1 in
+    let* t2_red = reduce t2 in
+    Ok (alpha_consistent reduce t1_red t2_red)
+  in
+  Result.fold ~ok:(fun x -> x) ~error:(fun _ -> false) res
 
-(** Checks if two terms are alpha consistent *)
-let rec alpha_consistent reduce t1 t2 : bool =
+and alpha_consistent reduce t1 t2 : bool =
   let open Ast in
-  let t1_red = reduce t1 |> Result.get_ok in
-  let t2_red = reduce t2 |> Result.get_ok in
-  match t1_red, t2_red with
+  match t1, t2 with
   | Var x, Var y -> x = y
   | Universe i, Universe j -> i = j
   | App (t1, u1), App (t2, u2) ->
-    alpha_consistent reduce t1 t2 && alpha_consistent reduce u1 u2
+    are_consistent reduce t1 t2 && are_consistent reduce u1 u2
   | Lambda fi1, Lambda fi2 ->
     let x_id = new_identifier () in
     let x = Var x_id in
     let body1 = subst1 fi1.id x fi1.body in
     let body2 = subst1 fi2.id x fi2.body in
-    alpha_consistent reduce fi1.dom fi2.dom && alpha_consistent reduce body1 body2
+    are_consistent reduce fi1.dom fi2.dom && are_consistent reduce body1 body2
   | Prod fi1, Prod fi2 ->
     let x_id = new_identifier () in
     let x = Var x_id in
     let body1 = subst1 fi1.id x fi1.body in
     let body2 = subst1 fi2.id x fi2.body in
-    alpha_consistent reduce fi1.dom fi2.dom && alpha_consistent reduce body1 body2
-  | _, Cast ci2 -> alpha_consistent reduce t1 ci2.term
-  | Cast ci1, _ -> alpha_consistent reduce ci1.term t2
+    are_consistent reduce fi1.dom fi2.dom && are_consistent reduce body1 body2
+  | _, Cast ci2 -> are_consistent reduce t1 ci2.term
+  | Cast ci1, _ -> are_consistent reduce ci1.term t2
   | _, Unknown _ -> true
   | Unknown _, _ -> true
   | Const x, Const y -> x = y
   | Inductive (ind1, i1, params1), Inductive (ind2, i2, params2) ->
-    ind1 = ind2 && i1 = i2 && List.for_all2 (alpha_consistent reduce) params1 params2
+    ind1 = ind2 && i1 = i2 && List.for_all2 (are_consistent reduce) params1 params2
   | Constructor c1, Constructor c2 ->
     c1.ctor = c2.ctor
     && c1.level = c2.level
-    && List.for_all2 (alpha_consistent reduce) c1.args c2.args
-    && List.for_all2 (alpha_consistent reduce) c1.params c2.params
+    && List.for_all2 (are_consistent reduce) c1.args c2.args
+    && List.for_all2 (are_consistent reduce) c1.params c2.params
   | Match m1, Match m2 ->
-    let alpha_consistent_branch = alpha_pred_branch (alpha_consistent reduce) in
-    alpha_consistent reduce m1.discr m2.discr
+    let are_consistent_branch b1 b2 =
+      let open Ast in
+      if List.compare_lengths b1.ids b2.ids = 0
+      then (
+        let new_ids = List.map (fun _ -> Var (new_identifier ())) b1.ids in
+        let ids1 = List.combine b1.ids new_ids in
+        let ids2 = List.combine b1.ids new_ids in
+        let subst_body body ids =
+          List.fold_left (fun body (old_id, new_id) -> subst1 old_id new_id body) body ids
+        in
+        let subst_body1 = subst_body b1.term ids1 in
+        let subst_body2 = subst_body b2.term ids2 in
+        b1.ctor = b2.ctor && are_consistent reduce subst_body1 subst_body2)
+      else false
+    in
+    are_consistent reduce m1.discr m2.discr
     && m1.ind = m2.ind
-    && alpha_consistent reduce m1.pred m2.pred
-    && List.equal alpha_consistent_branch m1.branches m2.branches
+    && are_consistent reduce m1.pred m2.pred
+    && List.equal are_consistent_branch m1.branches m2.branches
   | _ -> false
-
-let are_consistent reduce t1 t2 : bool = alpha_consistent reduce t1 t2
-(* let res =
-  in
-  Result.fold ~ok:(fun x -> x) ~error:(fun _ -> false) res *)
 
 (** The elaboration procedure, as per the paper *)
 let rec elaborate reduce ctx (term : Kernel.Ast.term)
